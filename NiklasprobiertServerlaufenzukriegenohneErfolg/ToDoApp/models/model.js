7c3f8778e4f0bf53c97ca61a35b64b1a32a5c237
Sumalvico <niklas.sumalvico@gmail.com>
2022-05-16 21:23:11 +0200

    ausprobieren

---
+++ NiklasprobiertServerlaufenzukriegenohneErfolg/ToDoApp/models/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    text: {
+        required: true,
+        type: String
+    },
+    due: {
+        required: true,
+        type: Date
+    }
+    ,
+    percent: {
+        required: true,
+        type: Number
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
