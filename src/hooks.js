860106aac8b7a018d4be8185dd5ab5e5c2020387
Thea Schöbl <wulkanat@gmail.com>
2022-05-20 14:17:46 +0200

    Revert "Zum ausprobieren"

--- src/hooks.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-const connection = mongoose.connect("mongodb://localhost:27017/todo")
-
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({event, resolve}) {
-  await connection
-  return resolve(event)
-}
